35d5b467cd9e53d746e8bc469782e862204d5808
Albert Ong <fluke807@gmail.com>
2020-05-06 23:18:31 -0400

    Added tensorflow.py skeleton

---
+++ tensorflow.py
@@ -0,0 +1,33 @@
+# Libraries
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
+%matplotlib inline
+np.random.seed(1)
+
+# Loss of one training set
+y_hat = tf.constant(36, name='y_hat')            # Define y_hat constant. Set to 36.
+y = tf.constant(39, name='y')                    # Define y. Set to 39
+
+loss = tf.Variable((y - y_hat)**2, name='loss')  # Create a variable for the loss
+
+init = tf.global_variables_initializer()         # When init is run later (session.run(init)),
+                                                 # the loss variable will be initialized and ready to be computed
+with tf.Session() as session:                    # Create a session and print the output
+    session.run(init)                            # Initializes the variables
+    print(session.run(loss))                     # Prints the loss
+
+# Create Tensors
+a = tf.constant(2)
+b = tf.constant(10)
+c = tf.multiply(a,b)
+print(c)
+
+# Create and run session
+sess = tf.Session()
+print(sess.run(c))
