414fa40158b9457144797a3858496c028a07927a
Albert Ong <fluke807@gmail.com>
2020-05-09 19:19:28 -0400

    Updated tensorflow.py

--- tensorflow.py
+++ tensorflow.py
@@ -92,3 +92,82 @@ def ones(shape):
     sess.close()
 
     return ones
+
+# Loading the dataset
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Flatten the training and test images
+X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+# Normalize image vectors
+X_train = X_train_flatten/255.
+X_test = X_test_flatten/255.
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6)
+Y_test = convert_to_one_hot(Y_test_orig, 6)
+
+# Creates the placeholders for the tensorflow session.
+def create_placeholders(n_x, n_y):
+    X = tf.placeholder(tf.float32, shape=[n_x, None], name='X')
+    Y = tf.placeholder(tf.float32, shape=[n_y, None], name='Y')
+
+    return X, Y
+    
+X, Y = create_placeholders(12288, 6)
+
+# To easily build neural networks
+def initialize_parameters():
+    tf.set_random_seed(1)                   
+    W1 = tf.get_variable("W1", [25, 12288], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b1 = tf.get_variable("b1", [25, 1], initializer=tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12, 25], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b2 = tf.get_variable("b2", [12, 1], initializer=tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [6, 12], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b3 = tf.get_variable("b3", [6, 1], initializer=tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
+
+tf.reset_default_graph()
+with tf.Session() as sess:
+    parameters = initialize_parameters()
+
+# model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+def forward_propagation(X, parameters):
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+    Z1 = tf.add(tf.matmul(W1, X), b1)                                     
+    A1 = tf.nn.relu(Z1)                                          
+    Z2 = tf.add(tf.matmul(W2, A1), b2)                                        
+    A2 = tf.nn.relu(Z2)                                 
+    Z3 = tf.add(tf.matmul(W3, A2), b3)                           
+    return Z3
+
+tf.reset_default_graph()
+with tf.Session() as sess:
+    X, Y = create_placeholders(12288, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+
+def compute_cost(Z3, Y):
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    return cost
+
+tf.reset_default_graph()
+with tf.Session() as sess:
+    X, Y = create_placeholders(12288, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    cost = compute_cost(Z3, Y)
